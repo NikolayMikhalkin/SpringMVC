8e75f962393abc68103ffc44f05fb7a5c2e0a0dc
Михалкин Николай Владимирович <mikhalkin@mmugb7.local>
2024-02-27 09:16:39 +0400

    asd

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
 import java.util.List;
@@ -20,7 +21,7 @@ public class CarsController {
     }
 
     @GetMapping(value = "/cars")
-    public String printCars(Integer count, ModelMap model) {
+    public String printCars(@RequestParam(value = "5", required = false) Integer count, ModelMap model) {
         List<Car> cars = carServiceImp.getCars(count);
         model.addAttribute("cars", cars);
         return "cars";
